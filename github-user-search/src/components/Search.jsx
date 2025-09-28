feb04a0c09323bfc5786a4234304afccb76d7eef
myriamleb9 <mlebbakher@gmail.com>
2025-09-28 16:41:40 +0100

    Fix: exact error string for ALX checker

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ const Search = () => {
       const userData = await fetchUserData(username);
       setUser(userData);
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
